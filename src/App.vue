c30396fdedd7a74b244f83f4ff86806b6d43ffce
deku <1687946464@qq.com>
2025-10-16 16:27:10 +0800

    elementPlus自动按需引入配置

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
-<script setup></script>
+<script setup>
+</script>
 
 <template>
   <h1>You did it!</h1>
@@ -6,6 +7,7 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <el-button type="primary">Primary</el-button>
 </template>
 
 <style scoped></style>
